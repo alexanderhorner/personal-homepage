01783bfa2d7c60f426204e5fa97bc0bfaed944b8
Alexander Horner <mail@alexanderhorner.com>
2022-03-05 16:36:54 +0100

    Testing cloudflare worker.

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,48 +2,54 @@
  * POST /api/submit
  */
 export async function onRequestPost(context) {
-    try {
-        let input = await context.request.formData()
-        let pretty = JSON.stringify([...input], null, 2)
-        return new Response(pretty, {
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            },
-        })
-      } catch (err) {
-        return new Response('Error parsing JSON content', { status: 400 })
-      }
+	try {
+		let input = await context.request.formData()
+		let pretty = JSON.stringify([...input], null, 2)
+	} catch (error) {
+		return new Response(`Error parsing JSON content: ${error}`, { status: 400 })
+	}
 
-    // await fetch('https://api.sendgrid.com/v3/mail/send', {
-    // 	body: JSON.stringify({
-    // 		"personalizations":[
-    // 			{
-    // 				"to":[
-    // 					{
-    // 						"email":"mail@alexanderhorner.com"
-    // 					}
-    // 				],
-    // 				"subject":"Hello, World!"
-    // 			}
-    // 		],
-    // 		"content":[
-    // 			{
-    // 				"type":"text/plain",
-    // 				"value":"Heya!"
-    // 			}
-    // 		],
-    // 		"from":{
-    // 			"email":"mail@alexanderhorner.com"
-    // 		},
-    // 		"reply_to":{
-    // 			"email":"sam@smith.com",
-    // 			"name":"Sam Smith"
-    // 		}
-    // 	}),
-    // 	headers: {
-    // 	'Authorization': `Bearer ${SENDGRID_API_KEY}`,
-    // 	'Content-Type': 'application/json',
-    // 	},
-    // 	method: 'POST',
-    // })
+	try {
+		let response = await fetch('https://api.sendgrid.com/v3/mail/send', {
+			body: JSON.stringify({
+				"personalizations":[
+					{
+						"to":[
+							{
+								"email":"mail@alexanderhorner.com"
+							}
+						],
+						"subject":"Hello, World!"
+					}
+				],
+				"content":[
+					{
+						"type":"text/plain",
+						"value":"Heya!"
+					}
+				],
+				"from":{
+					"email":"mail@alexanderhorner.com"
+				},
+				"reply_to":{
+					"email":"sam@smith.com",
+					"name":"Sam Smith"
+				}
+			}),
+			headers: {
+			'Authorization': `Bearer ${SENDGRID_API_KEY}`,
+			'Content-Type': 'application/json',
+			},
+			method: 'POST',
+		})
+	} catch (error) {
+		return new Response(`Error sending email: ${error}`, { status: 400 })
+	}
+
+	return new Response({ ...pretty, ...response }, {
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+		},
+	})
+	
 }
