e1f4fa4fb516651e9f95408e53df82c3390cddcd
Alexander Horner <mail@alexanderhorner.com>
2022-03-05 16:19:50 +0100

    Testing cloudflare worker.

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -3,11 +3,16 @@
  */
 export async function onRequestPost(context) {
     try {
-        let input = await context.request.formData();
-        let pretty = JSON.stringify([...input], null, 2);
-    } catch (err) {
-        return new Response('Error parsing JSON content', { status: 400 });
-    }
+        let input = await context.request.formData()
+        let pretty = JSON.stringify([...input], null, 2)
+        return new Response(pretty, {
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+        })
+      } catch (err) {
+        return new Response('Error parsing JSON content', { status: 400 })
+      }
 
     // await fetch('https://api.sendgrid.com/v3/mail/send', {
     // 	body: JSON.stringify({
