e79ad1fb96634473a2d100ca21816f17c0ebc33a
Alexander Horner <mail@alexanderhorner.com>
2022-03-07 11:44:40 +0100

    Fix form api validation

--- src/js/form.ts
+++ src/js/form.ts
@@ -20,9 +20,7 @@ form.addEventListener("submit", (e) => {
         method: 'post',
         body: formData,
     }).then(response => {
-            console.log(response.status)
-            console.log(response.status[0])
-            if (response.status[0] != 2) {
+            if (response.status.toString().startsWith("2")) {
                 throw 'HTTP Status code is not 2xx';
             } 
             return response
