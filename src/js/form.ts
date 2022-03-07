6064585cc80606cb75c2179cd5062a95481435bc
Alexander Horner <mail@alexanderhorner.com>
2022-03-07 10:48:17 +0100

    Fix form error.

--- src/js/form.ts
+++ src/js/form.ts
@@ -20,8 +20,8 @@ form.addEventListener("submit", (e) => {
             // @ts-ignore
             document.querySelector(".success").style.display = "block"
             // @ts-ignore
-            document.querySelector(".form").style.display = "none"
-            
+            document.querySelector("form").style.display = "none"
+
             console.log(response.json())
         })
         .catch(error => {
